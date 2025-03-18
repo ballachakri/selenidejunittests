1724f7780014b8cec5eacc530d98340545816c6e
Chakra <ballachakri@yahoo.com>
2025-03-18 13:51:53 +0000

    second commit

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -6,33 +6,31 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.SearchPage;
-import java.util.List;
 
+import java.util.List;
 
 
 public class SearchTests extends Hooks {
 
-    SearchPage searchPage =new SearchPage();
+    SearchPage searchPage = new SearchPage();
 
     @Test
     @DisplayName("User search for iphone")
-    public void searchIphone(){
+    public void searchIphone() {
 
-      searchPage.searchItem("Iphone");
-
+        searchPage.searchItem("Iphone");
 
         List<SelenideElement> list = searchPage.getSearchResultsList();
 
         for (SelenideElement selenideElement : list) {
-           Assertions.assertThat(selenideElement.getText()).contains("iPhone");
+            Assertions.assertThat(selenideElement.getText()).contains("iPhone");
         }
     }
 
 
-
     @Test
     @DisplayName("User search for mac")
-    public void searchMac(){
+    public void searchMac() {
 
         searchPage.searchItem("mac");
 
